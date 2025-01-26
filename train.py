9cdded1930386ac9c802df9879559e93c76b9b67
Varsha Gupta <vg4871@srmist.edu.in>
2025-01-27 00:01:53 +0530

    Updated

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
-try: [tf.config.experimental.set_memory_growth(gpu, True) for gpu in tf.config.experimental.list_physical_devices("GPU")]
-except: pass
+try:
+    [tf.config.experimental.set_memory_growth(gpu, True) for gpu in tf.config.experimental.list_physical_devices("GPU")]
+except:
+    pass
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 
 from mltu.preprocessors import ImageReader
-from mltu.transformers import ImageResizer, LabelIndexer, LabelPadding, ImageShowCV2
+from mltu.transformers import ImageResizer, LabelIndexer, LabelPadding
 from mltu.augmentors import RandomBrightness, RandomRotate, RandomErodeDilate, RandomSharpen
 from mltu.annotations.images import CVImage
 
@@ -24,7 +26,6 @@ from urllib.request import urlopen
 from io import BytesIO
 from zipfile import ZipFile
 
-
 def download_and_unzip(url, extract_to="Datasets", chunk_size=1024*1024):
     http_response = urlopen(url)
 
@@ -36,102 +37,108 @@ def download_and_unzip(url, extract_to="Datasets", chunk_size=1024*1024):
     zipfile = ZipFile(BytesIO(data))
     zipfile.extractall(path=extract_to)
 
-dataset_path = os.path.join("Datasets", "IAM_Words")
-if not os.path.exists(dataset_path):
-    download_and_unzip("https://git.io/J0fjL", extract_to="Datasets")
+def prepare_dataset():
+    dataset_path = os.path.join("Datasets", "IAM_Words")
+    if not os.path.exists(dataset_path):
+        download_and_unzip("https://git.io/J0fjL", extract_to="Datasets")
 
-    file = tarfile.open(os.path.join(dataset_path, "words.tgz"))
-    file.extractall(os.path.join(dataset_path, "words"))
+        file = tarfile.open(os.path.join(dataset_path, "words.tgz"))
+        file.extractall(os.path.join(dataset_path, "words"))
 
-dataset, vocab, max_len = [], set(), 0
+    dataset, vocab, max_len = [], set(), 0
 
-# Preprocess the dataset by the specific IAM_Words dataset file structure
-words = open(os.path.join(dataset_path, "words.txt"), "r").readlines()
-for line in tqdm(words):
-    if line.startswith("#"):
-        continue
+    words = open(os.path.join(dataset_path, "words.txt"), "r").readlines()
+    for line in tqdm(words):
+        if line.startswith("#") or line.split(" ")[1] == "err":
+            continue
 
-    line_split = line.split(" ")
-    if line_split[1] == "err":
-        continue
+        line_split = line.split(" ")
+        folder1 = line_split[0][:3]
+        folder2 = "-".join(line_split[0].split("-")[:2])
+        file_name = line_split[0] + ".png"
+        label = line_split[-1].rstrip("\n")
 
-    folder1 = line_split[0][:3]
-    folder2 = "-".join(line_split[0].split("-")[:2])
-    file_name = line_split[0] + ".png"
-    label = line_split[-1].rstrip("\n")
+        rel_path = os.path.join(dataset_path, "words", folder1, folder2, file_name)
+        if not os.path.exists(rel_path):
+            print(f"File not found: {rel_path}")
+            continue
 
-    rel_path = os.path.join(dataset_path, "words", folder1, folder2, file_name)
-    if not os.path.exists(rel_path):
-        print(f"File not found: {rel_path}")
-        continue
+        dataset.append([rel_path, label])
+        vocab.update(list(label))
+        max_len = max(max_len, len(label))
 
-    dataset.append([rel_path, label])
-    vocab.update(list(label))
-    max_len = max(max_len, len(label))
+    return dataset, vocab, max_len
 
-# Create a ModelConfigs object to store model configurations
-configs = ModelConfigs()
+def main():
+    # Prepare dataset
+    dataset, vocab, max_len = prepare_dataset()
 
-# Save vocab and maximum text length to configs
-configs.vocab = "".join(vocab)
-configs.max_text_length = max_len
-configs.save()
+    # Create a ModelConfigs object to store model configurations
+    configs = ModelConfigs()
 
-# Create a data provider for the dataset
-data_provider = DataProvider(
-    dataset=dataset,
-    skip_validation=True,
-    batch_size=configs.batch_size,
-    data_preprocessors=[ImageReader(CVImage)],
-    transformers=[
-        ImageResizer(configs.width, configs.height, keep_aspect_ratio=False),
-        LabelIndexer(configs.vocab),
-        LabelPadding(max_word_length=configs.max_text_length, padding_value=len(configs.vocab)),
+    # Save vocab and maximum text length to configs
+    configs.vocab = "".join(vocab)
+    configs.max_text_length = max_len
+    configs.save()
+
+    # Create a data provider for the dataset
+    data_provider = DataProvider(
+        dataset=dataset,
+        skip_validation=True,
+        batch_size=configs.batch_size,
+        data_preprocessors=[ImageReader(CVImage)],
+        transformers=[
+            ImageResizer(configs.width, configs.height, keep_aspect_ratio=False),
+            LabelIndexer(configs.vocab),
+            LabelPadding(max_word_length=configs.max_text_length, padding_value=len(configs.vocab)),
         ],
-)
+    )
 
-# Split the dataset into training and validation sets
-train_data_provider, val_data_provider = data_provider.split(split = 0.9)
+    # Split the dataset into training and validation sets
+    train_data_provider, val_data_provider = data_provider.split(split=0.9)
 
-# Augment training data with random brightness, rotation and erode/dilate
-train_data_provider.augmentors = [
-    RandomBrightness(),
-    RandomErodeDilate(),
-    RandomSharpen(),
-    RandomRotate(angle=10),
+    # Augment training data with random brightness, rotation and erode/dilate
+    train_data_provider.augmentors = [
+        RandomBrightness(),
+        RandomErodeDilate(),
+        RandomSharpen(),
+        RandomRotate(angle=10),
     ]
 
-# Creating TensorFlow model architecture
-model = train_model(
-    input_dim = (configs.height, configs.width, 3),
-    output_dim = len(configs.vocab),
-)
+    # Creating TensorFlow model architecture
+    model = train_model(
+        input_dim=(configs.height, configs.width, 3),
+        output_dim=len(configs.vocab),
+    )
 
-# Compile the model and print summary
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=configs.learning_rate),
-    loss=CTCloss(),
-    metrics=[CWERMetric(padding_token=len(configs.vocab))],
-)
-model.summary(line_length=110)
+    # Compile the model and print summary
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=configs.learning_rate),
+        loss=CTCloss(),
+        metrics=[CWERMetric(padding_token=len(configs.vocab))],
+    )
+    model.summary(line_length=110)
 
-# Define callbacks
-earlystopper = EarlyStopping(monitor="val_CER", patience=20, verbose=1)
-checkpoint = ModelCheckpoint(f"{configs.model_path}/model.h5", monitor="val_CER", verbose=1, save_best_only=True, mode="min")
-trainLogger = TrainLogger(configs.model_path)
-tb_callback = TensorBoard(f"{configs.model_path}/logs", update_freq=1)
-reduceLROnPlat = ReduceLROnPlateau(monitor="val_CER", factor=0.9, min_delta=1e-10, patience=10, verbose=1, mode="auto")
-model2onnx = Model2onnx(f"{configs.model_path}/model.h5")
+    # Define callbacks
+    earlystopper = EarlyStopping(monitor="val_CER", patience=20, verbose=1)
+    checkpoint = ModelCheckpoint(f"{configs.model_path}/model.keras", monitor="val_CER", verbose=1, save_best_only=True, mode="min")
+    trainLogger = TrainLogger(configs.model_path)
+    tb_callback = TensorBoard(f"{configs.model_path}/logs", update_freq=1)
+    reduceLROnPlat = ReduceLROnPlateau(monitor="val_CER", factor=0.9, min_delta=1e-10, patience=10, verbose=1, mode="auto")
+    model2onnx = Model2onnx(f"{configs.model_path}/model.keras")
 
-# Train the model
-model.fit(
-    train_data_provider,
-    validation_data=val_data_provider,
-    epochs=configs.train_epochs,
-    callbacks=[earlystopper, checkpoint, trainLogger, reduceLROnPlat, tb_callback, model2onnx],
-    workers=configs.train_workers
-)
+    # Train the model
+    model.fit(
+        train_data_provider,
+        validation_data=val_data_provider,
+        epochs=configs.train_epochs,
+        callbacks=[earlystopper, checkpoint, trainLogger, reduceLROnPlat, tb_callback, model2onnx],
+        workers=configs.train_workers
+    )
 
-# Save training and validation datasets as csv files
-train_data_provider.to_csv(os.path.join(configs.model_path, "train.csv"))
-val_data_provider.to_csv(os.path.join(configs.model_path, "val.csv"))
+    # Save training and validation datasets as csv files
+    train_data_provider.to_csv(os.path.join(configs.model_path, "train.csv"))
+    val_data_provider.to_csv(os.path.join(configs.model_path, "val.csv"))
+
+if __name__ == "__main__":
+    main()
